d621c7182932ad343c8581178c49d7c07c394362
Choi Kim <choco920@gmail.com>
2022-12-14 00:19:40 +0900

    empty component 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import Empty from './Empty';
 
 const TodoList = ({ todos }) => {
   console.log(todos);
   return (
     <div>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          content={todo.content}
-          isComplete={todo.isComplete}
-        />
-      ))}
+      {/* todos의 길이가 0이면 empty, 1이상이면 todos.map todoitem */}
+      {todos.length === 0 ? (
+        <Empty />
+      ) : (
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            content={todo.content}
+            isComplete={todo.isComplete}
+          />
+        ))
+      )}
     </div>
   );
 };
